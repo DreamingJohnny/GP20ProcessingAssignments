368bac68c2b383c9a68959a1ba498234a6d3c91c
DreamingJohnny <johnnydreaming@gmail.com>
2020-09-08 19:51:46 +0200

    Letters are now functions, dimensions of letters are proportional

--- Assignment01/Assignment01.pde
+++ Assignment01/Assignment01.pde
@@ -1,43 +1,65 @@
+String nameToPrint = "johan";
+float topOfLetterY, bottomOfLetterY, widthOfLetterX, middleOfScreenOnX;
+
 void setup(){
 
   size(768,432);
   background(#390155);
-  
+  stroke(0);
+  strokeWeight(3);
+  noFill();
+
+  //To ensure positioning and dimensions of letters is relative to screensize
+  topOfLetterY = height/3;
+  widthOfLetterX = width/(nameToPrint.length()+4);
+
 }void draw(){ 
 
-//Todo: Do first ugly code in the draw function the letters.
-//TODO: Make sure your able to open with processing and nothing wrong there.
-//TODO: Send up to repository @ Github, see if that works, send link, promise improvment.
-//TODO: Pull branch, make changes.
-//TODO: Set float var as positioning for letters proportional to dimensions of screen.
-//TODO: Set function for each letter that you may send float position to.
-//TODO: Set better looking size of lettering
 //TODO: Look into animating letters, possible to have them drawn when mousepos changes?
 //TODO: Look into curving letters as inspired by kalligraphy, means making shapes?
 
-stroke(0);
-noFill();
+drawLetterJ(widthOfLetterX,topOfLetterY);
 
+drawLetterO(widthOfLetterX,topOfLetterY);
+
+drawLetterH(widthOfLetterX,topOfLetterY);
+
+drawLetterA(widthOfLetterX,topOfLetterY);
+
+drawLetterN(widthOfLetterX,topOfLetterY);
+  
+}
 //Letter J
-line(100,50,150,50);
-line(150,50,150,120);
-arc(125, 120, 50, 50, 0, PI);
-
+void drawLetterJ (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*2, heightOfLetterY,widthOfLetterX*3, heightOfLetterY);
+	line(widthOfLetterX*3,heightOfLetterY,widthOfLetterX*3,heightOfLetterY*1.75);
+	arc(widthOfLetterX*2.5,heightOfLetterY*1.75, widthOfLetterX, widthOfLetterX, 0, PI);
+}
 //Letter "O"
-ellipse(180, 100, 50, 100);
+void drawLetterO(float widthOfLetterX, float heightOfLetterY)
+{
+	ellipse(widthOfLetterX*3.5, heightOfLetterY*1.5,widthOfLetterX-(widthOfLetterX/10),heightOfLetterY);
 
-//Letter "H"
-line(220,50,220,150);
-line(220,100,250,100);
-line(250,50,250,150);
-
+}
+//Letter H
+void drawLetterH (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*4,heightOfLetterY,widthOfLetterX*4,heightOfLetterY*2);
+	line(widthOfLetterX*5,heightOfLetterY,widthOfLetterX*5,heightOfLetterY*2);
+	line(widthOfLetterX*4,heightOfLetterY*1.5,widthOfLetterX*5,heightOfLetterY*1.5);
+}
 //Letter "A"
-line(260,150,285,50);
-line(285,50,310,150);
-line(260,100,310,100);
-
+void drawLetterA (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*5,heightOfLetterY*2,widthOfLetterX*5.5,heightOfLetterY);
+	line(widthOfLetterX*5.5,heightOfLetterY,widthOfLetterX*6,heightOfLetterY*2);
+	line(widthOfLetterX*5.2,heightOfLetterY*1.5,widthOfLetterX*5.8,heightOfLetterY*1.5);
+}
 //Letter "N"
-line(320,150,320,50);
-line(320,50,370,150);
-line(370,150,370,50);  
+void drawLetterN(float widthOfLetterX, float  heightOfLetterY)
+{
+	line(widthOfLetterX*6,heightOfLetterY*2,widthOfLetterX*6,heightOfLetterY);
+	line(widthOfLetterX*6,heightOfLetterY,widthOfLetterX*7,heightOfLetterY*2);
+	line(widthOfLetterX*7,heightOfLetterY*2,widthOfLetterX*7,heightOfLetterY);
 }
--- Assignment01/build-tmp/source/Assignment01.java
+++ Assignment01/build-tmp/source/Assignment01.java
@@ -14,40 +14,84 @@ import java.io.IOException;
 
 public class Assignment01 extends PApplet {
 
+String nameToPrint = "johan";
+float topOfLetterY, bottomOfLetterY, widthOfLetterX, middleOfScreenOnX;
+
 public void setup(){
 
   
   background(0xff390155);
-  
+  stroke(0);
+  strokeWeight(3);
+  noFill();
+
+  //To ensure positioning and dimensions of letters is relative to screensize
+  topOfLetterY = height/3;
+
+  //Divide this in to more parts... but that means I have to increase the amount of x in each x of course hm...
+  widthOfLetterX = width/(nameToPrint.length()+4);
+
 }public void draw(){ 
 
-//Todo: Do first ugly hardcode in the draw function the letters.
+//Todo: Do first ugly code in the draw function the letters.
+//TODO: Make sure your able to open with processing and nothing wrong there.
+//TODO: Send up to repository @ Github, see if that works, send link, promise improvment.
+//TODO: Pull branch, make changes.
 
-stroke(0);
-noFill();
+//TODO: Set float var as positioning for letters proportional to dimensions of screen.
+//TODO: Set function for each letter that you may send float position to.
+//TODO: fix thickness of letters
 
+//TODO: Set better looking size of lettering
+//TODO: Look into animating letters, possible to have them drawn when mousepos changes?
+//TODO: Look into curving letters as inspired by kalligraphy, means making shapes?
+
+
+
+drawLetterJ(widthOfLetterX,topOfLetterY);
+
+drawLetterO(widthOfLetterX,topOfLetterY);
+
+drawLetterH(widthOfLetterX,topOfLetterY);
+
+drawLetterA(widthOfLetterX,topOfLetterY);
+
+drawLetterN(widthOfLetterX,topOfLetterY);
+  
+}
 //Letter J
-line(100,50,150,50);
-line(150,50,150,120);
-arc(125, 120, 50, 50, 0, PI);
-
+public void drawLetterJ (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*2, heightOfLetterY,widthOfLetterX*3, heightOfLetterY);
+	line(widthOfLetterX*3,heightOfLetterY,widthOfLetterX*3,heightOfLetterY*1.75f);
+	arc(widthOfLetterX*2.5f,heightOfLetterY*1.75f, widthOfLetterX, widthOfLetterX, 0, PI);
+}
 //Letter "O"
-ellipse(180, 100, 50, 100);
+public void drawLetterO(float widthOfLetterX, float heightOfLetterY)
+{
+	ellipse(widthOfLetterX*3.5f, heightOfLetterY*1.5f,widthOfLetterX-(widthOfLetterX/10),heightOfLetterY);
 
-//Letter "H"
-line(220,50,220,150);
-line(220,100,250,100);
-line(250,50,250,150);
-
+}
+//Letter H
+public void drawLetterH (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*4,heightOfLetterY,widthOfLetterX*4,heightOfLetterY*2);
+	line(widthOfLetterX*5,heightOfLetterY,widthOfLetterX*5,heightOfLetterY*2);
+	line(widthOfLetterX*4,heightOfLetterY*1.5f,widthOfLetterX*5,heightOfLetterY*1.5f);
+}
 //Letter "A"
-line(260,150,285,50);
-line(285,50,310,150);
-line(260,100,310,100);
-
+public void drawLetterA (float widthOfLetterX, float heightOfLetterY)
+{
+	line(widthOfLetterX*5,heightOfLetterY*2,widthOfLetterX*5.5f,heightOfLetterY);
+	line(widthOfLetterX*5.5f,heightOfLetterY,widthOfLetterX*6,heightOfLetterY*2);
+	line(widthOfLetterX*5.2f,heightOfLetterY*1.5f,widthOfLetterX*5.8f,heightOfLetterY*1.5f);
+}
 //Letter "N"
-line(320,150,320,50);
-line(320,50,370,150);
-line(370,150,370,50);  
+public void drawLetterN(float widthOfLetterX, float  heightOfLetterY)
+{
+	line(widthOfLetterX*6,heightOfLetterY*2,widthOfLetterX*6,heightOfLetterY);
+	line(widthOfLetterX*6,heightOfLetterY,widthOfLetterX*7,heightOfLetterY*2);
+	line(widthOfLetterX*7,heightOfLetterY*2,widthOfLetterX*7,heightOfLetterY);
 }
   public void settings() {  size(768,432); }
   static public void main(String[] passedArgs) {
