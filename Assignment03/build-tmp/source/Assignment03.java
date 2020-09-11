aa930ec7e75fc10adb9a902c44d89a20e70041aa
DreamingJohnny <johnnydreaming@gmail.com>
2020-09-11 16:15:56 +0200

    First and second pass on Assignment03, must fix
    - the ball must gain friction and become still when speed is below a certain value.
    - it has a weird way of moving now that needs to be solved, probably just a question of moving code arround
    - the line I've added to show the projecting also should be moved to an if statement.
    - rebound of edge

--- Assignment03/build-tmp/source/Assignment03.java
+++ Assignment03/build-tmp/source/Assignment03.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Assignment03 extends PApplet {
+
 float circleDiamater, circleFriction, toDivideBy;
 
 PVector circlePositioning = new PVector();
@@ -5,10 +21,10 @@ PVector circleGoal = new PVector();
 PVector circleVelocity = new PVector();
 PVector circleDrag = new PVector();
 
-void setup() {
+public void setup() {
 
 	//Design
-	size(512, 512);
+	
 	background(255,255,255);
 	stroke(0);
 
@@ -24,7 +40,7 @@ void setup() {
 
 }
 
-void draw() {
+public void draw() {
 	
 	background(255,255,255);
 
@@ -38,7 +54,7 @@ void draw() {
 
 	//Might be easiest to fix circle vibrating at edge with a simple "if velocity lower than 2 make velocity 0"
 }
-void mouseClicked() {
+public void mouseClicked() {
 	//Do I really need to recreate the vector like this?
 	circlePositioning = new PVector(mouseX,mouseY);
 
@@ -47,12 +63,12 @@ void mouseClicked() {
 	//circlePositioning.y = mouseY;
 
 }
-void mouseDragged() {
+public void mouseDragged() {
 	//Can processing sort this out? or do I need to divide it up for it to work?
 	//TODO check if an if statement, in draw will help line to not vibrate
 	line(circlePositioning.x,circlePositioning.y,mouseX,mouseY);
 }
-void mouseReleased() {
+public void mouseReleased() {
 	//Does 
 	//So, this will save the vector for the position where user lets go of the mouse.
 	circleGoal = new PVector(mouseX,mouseY);
@@ -61,4 +77,14 @@ void mouseReleased() {
 	circleVelocity = circleVelocity.div(toDivideBy);
 
 	
+}
+  public void settings() { 	size(512, 512); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Assignment03" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
